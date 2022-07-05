92f63455d3f8ec95f66e601a8b61cb01cc57409c
mitsuumi <j6gjvm00@gmail.com>
2022-07-05 15:12:41 +0800

    導入bootstrap

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,19 @@
+import { navbarData } from './nav-data';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  collapsed = false;
+  navData = navbarData;
+
+}
